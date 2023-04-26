cbf999015dccdc8f766bc74d86d85b48bdf53ff3
ankush.tyagi <ankush.tyagi@squareboat.com>
2023-04-26 16:32:40 +0530

    Job:implement JWT, create login,signup,delete api

--- laravel/database/migrations/2023_04_26_094039_create_jobs_table.php
+++ laravel/database/migrations/2023_04_26_094039_create_jobs_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role')->default(1)->comment('0->admin 1->user 2->recruiter');
-            $table->integer('status')->default(0)->comment('0->active 1->delete');
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
             $table->string('ulid');
+            $table->string('job_title');
+            $table->string('job_description');
+            $table->integer('recruiter_id');
+            $table->integer('status')->default(0)->comment('0->active 1-> delete');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jobs');
     }
 };
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
 
+// });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('auth/login', 'login');
+    Route::post('auth/signup', 'register');
+    Route::post('auth/logoutlogout', 'logout');
 });
-Route::get('/users',[Controller::class,'users']);
+
+Route::group(['middleware' => ['auth:api']], function() {
+    Route::get('/users',[Controller::class,'users']);
+});
